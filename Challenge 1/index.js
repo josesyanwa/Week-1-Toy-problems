01dd3485bff89183b19fdb6ef44d70343f1d6b81
Joses Syanwa <josesyanwa@gmail.com>
2023-10-08 12:26:43 +0300

    Completed Challenge 1

---
+++ Challenge 1/index.js
@@ -0,0 +1,13 @@
+function calculateGrade(mark) {
+    if (mark > 79) {
+        return 'A';
+    } else if (mark >= 60) {
+        return 'B';
+    } else if (mark >= 50) {
+        return 'C';
+    } else if (mark >= 40) {
+        return 'D';
+    } else {
+        return 'E';
+    }
+}
